deb71f4aa32f1a943b6ce4ebe55110de004f101b
Alex Cho <acho20@stuy.edu>
2020-11-22 17:59:11 -0500

    created add()

--- RealNumber.java
+++ RealNumber.java
@@ -32,8 +32,7 @@ public class RealNumber{
   public RealNumber add(RealNumber other){
      //other can be ANY RealNumber, including a RationalNumber
      //or other subclasses of RealNumber (that aren't written yet)
-
-     return null;
+     return value + other.getValue();
   }
 
   /*
