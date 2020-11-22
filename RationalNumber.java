b83844c009af888f573aa2add4eaa806ee3ea2a5
Alex Cho <acho20@stuy.edu>
2020-11-22 18:30:02 -0500

    added multiply() and divide()

--- RationalNumber.java
+++ RationalNumber.java
@@ -80,14 +80,18 @@ public class RationalNumber extends RealNumber {
   *Return a new RationalNumber that is the product of this and the other
   */
   public RationalNumber multiply(RationalNumber other){
-    return null;
+    RationalNumber x = new RationalNumber(numerator * other.getNumerator(), denominator * other.getDenominator());
+    reduce();
+    return x;
   }
 
   /**
   *Return a new RationalNumber that is the this divided by the other
   */
   public RationalNumber divide(RationalNumber other){
-    return null;
+    RationalNumber x = new RationalNumber(numerator * other.getDenominator(), denominator * other.getNumerator());
+    reduce();
+    return x;
   }
 
   /**
